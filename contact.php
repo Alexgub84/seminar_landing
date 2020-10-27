efe158773d053304fb54cdc853bd3f3d3147e71d
Alex <alex.gub@gmail.com>
2020-10-27 15:22:07 +0200

    ds

--- contact.php
+++ contact.php
@@ -1,25 +1,25 @@
 <?php
-    // $name = $_POST['name'];
-    // $lastname= $_POST['lastname'];
-    // $phone= $_POST['phone'];
-    // $email= $_POST['email'];
-    // $address= $_POST['address'];
-    // $city= $_POST['city'];
-    // $participation= $_POST['participation'];
+    $name = $_POST['name'];
+    $lastname= $_POST['lastname'];
+    $phone= $_POST['phone'];
+    $email= $_POST['email'];
+    $address= $_POST['address'];
+    $city= $_POST['city'];
+    $participation= $_POST['participation'];
 
-    // $email_from='Landmark Registration site';
-    // $email_subject='Seminar Registration';
-    // $email_body = "Name: $name.\n".
-    //              "Email: $email.\n".
-    //              "Participation history: $participation.\n";
+    $email_from='Landmark Registration site';
+    $email_subject='Seminar Registration';
+    $email_body = "Name: $name.\n".
+                 "Email: $email.\n".
+                 "Participation history: $participation.\n";
 
-    // $to="alex.gub@gmail.com";
-    // mail($to,$email_subject,$email_body);
-    // header("location: home.html");
+    $headers="Seminar reg";
+    $to="alex.gub@gmail.com";
+    mail($to,$email_subject,$email_body);
+    header("location: home.html");
 
     $msg = "try";
-    $headers="Seminar reg";
-    mail("alex.gub@gmail.com","subject",$msg,$headers);
+
     if (mail("alex.gub@gmail.com","subject",$msg,$headers))
     {
         echo "Message accepted";
