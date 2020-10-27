7607b881c13d2238f7d013d29b6f53e7fffaa039
Alex <alex.gub@gmail.com>
2020-10-27 12:42:23 +0200

    form and php

--- contact.php
+++ contact.php
@@ -1,17 +1,22 @@
 <?php
-    $name = $_POST['name'];
-    $lastname= $_POST['lastname'];
-    $phone= $_POST['phone'];
-    $email= $_POST['email'];
-    $address= $_POST['address'];
-    $city= $_POST['city'];
-    $participation= $_POST['participation'];
+    // $name = $_POST['name'];
+    // $lastname= $_POST['lastname'];
+    // $phone= $_POST['phone'];
+    // $email= $_POST['email'];
+    // $address= $_POST['address'];
+    // $city= $_POST['city'];
+    // $participation= $_POST['participation'];
 
+    // $email_from='Landmark Registration site';
+    // $email_subject='Seminar Registration';
+    // $email_body = "Name: $name.\n".
+    //              "Email: $email.\n".
+    //              "Participation history: $participation.\n";
     $email_from='Landmark Registration site';
     $email_subject='Seminar Registration';
-    $email_body = "Name: $name.\n".
-                 "Email: $email.\n".
-                 "Participation history: $participation.\n";
+    $email_body = "Name: Alex.\n".
+                 "Email: bla@bla.\n".
+                 "Participation history: Hero.\n";
 
     $to="alex.gub@gmail.com";
     mail($to,$email_subject,$email_body);
