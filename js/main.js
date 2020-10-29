7be7e331c721445548fba3bbb8714fd54b6e42db
Alex <alex.gub@gmail.com>
2020-10-29 13:35:36 +0200

    sd

--- js/main.js
+++ js/main.js
@@ -9,27 +9,24 @@ function init() {
     alert.href = PAYMENT_URL;
     const form = document.querySelector('form');
     form.addEventListener('submit', (ev) => {
-        // ev.preventDefault();
-        // ev.stopPropagation()
-        debugger
+        ev.preventDefault();
         if (!validate()) return
         $('#alert').show();
-        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=0,height=0,left=-1000,top=-1000`;
-        setTimeout(() => {
-            window.open(PAYMENT_URL,'Seminar payment',params)
-        }, 2000)
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: this.encode({
+              "form-name": "ask-question",
+              "form_field": "something"
+            })
+          })
+            .then(() => {
+                let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=0,height=0,left=-1000,top=-1000`;
+                setTimeout(() => {
+                    window.open(PAYMENT_URL,'Seminar payment',params)
+                }, 2000)            })
+        
 
-        const formData = new FormData(form);
-        fetch('contact.php',{
-            method:'post',
-            body: FormData
-        })
-            .then(res=>{
-                console.log(res);
-            })
-            .catch(err=>{
-                console.log('error is',err);
-            })
     })
 
 }
@@ -44,5 +41,5 @@ function validate() {
 function onRegBtnPressed(){
     $('html, body').animate({
         scrollTop: $("#reg-form").offset().top
-    }, 2000);
+    }, 1000);
 }
