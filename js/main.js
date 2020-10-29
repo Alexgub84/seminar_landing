e587fab5b6e7a96366ac91b5391475f2d72709d1
Alex <alex.gub@gmail.com>
2020-10-29 13:21:03 +0200

    sd

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ function init() {
     alert.href = PAYMENT_URL;
     const form = document.querySelector('form');
     form.addEventListener('submit', (ev) => {
-        ev.preventDefault();
-        ev.stopPropagation()
+        // ev.preventDefault();
+        // ev.stopPropagation()
         if (!validate()) return
         $('#alert').show();
         let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=0,height=0,left=-1000,top=-1000`;
