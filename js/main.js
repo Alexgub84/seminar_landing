ef7fd7dca17a8cb4b745f604cd7dca1d2b506006
Alex <alex.gub@gmail.com>
2021-01-20 16:56:29 +0200

    d

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
-'use strict'
+"use strict";
 
-const PAYMENT_URL = 'https://ng.paymeservice.com/sale/template/SALE1592-212720GX-BZVSLYQP-6P42ANSC'
-
-
+const PAYMENT_URL =
+  "https://ng.paymeservice.com/sale/template/SALE1592-212720GX-BZVSLYQP-6P42ANSC";
 
 function init() {
-   const alert = document.querySelector('.alert-link');
-    alert.href = PAYMENT_URL;
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (ev) => {
-        ev.preventDefault();
-        if (!validate()) return
-        $('#alert').show();
+  const alert = document.querySelector(".alert-link");
+  alert.href = PAYMENT_URL;
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    if (!validate()) return;
+    $("#alert").show();
 
-        const formData = new FormData(form);
+    const formData = new FormData(form);
 
-        
-        fetch(form.getAttribute('action'), {
-            method: "POST",
-            headers: {
-                'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-              },
-              body: new URLSearchParams(formData).toString()
-            })
-            .then(() => {
-                let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=0,height=0,left=-1000,top=-1000`;
-                setTimeout(() => {
-                    window.open(PAYMENT_URL,'Seminar payment',params)
-                }, 2000)            })
-            .catch(() => {
-                    console.log();('failed');
-            })   
-
+    fetch(form.getAttribute("action"), {
+      method: "POST",
+      headers: {
+        Accept: "application/x-www-form-urlencoded;charset=UTF-8",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: new URLSearchParams(formData).toString(),
     })
-
+      .then(() => {
+        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no, width=0,height=0,left=-1000,top=-1000`;
+        setTimeout(() => {
+          window.open(PAYMENT_URL, "Seminar payment", params);
+        }, 2000);
+      })
+      .catch(() => {
+        console.log();
+        ("failed");
+      });
+  });
 }
 
 function validate() {
-    const form = document.querySelector('.needs-validation');
-    console.log(form);
-    form.classList.add('was-validated');
-    return form.checkValidity()
+  const form = document.querySelector(".needs-validation");
+  console.log(form);
+  form.classList.add("was-validated");
+  return form.checkValidity();
 }
 
-function onRegBtnPressed(){
-    $('html, body').animate({
-        scrollTop: $("#reg-form").offset().top
-    }, 1000);
+function onRegBtnPressed() {
+  $("html, body").animate(
+    {
+      scrollTop: $("#reg-form").offset().top,
+    },
+    1000
+  );
 }
